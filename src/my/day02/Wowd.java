821ede67e1fe7ab4682b26191c66fef216cbbdac
414 <414@DESKTOP-D2GFAIH>
2018-07-03 16:06:41 +0900

    구구단

---
+++ src/my/day02/Wowd.java
@@ -0,0 +1,21 @@
+package my.day02;
+
+public class Wowd {
+
+	public static void main(String[] args) {
+			 
+		int result = 0;
+		
+	for (int i = 1; i < 10; i++) {
+		
+		for (int n = 1; n < 10; n++) {
+			
+			result = i * n;
+			System.out.println(i+ " * " +n+ " = " +result);
+		}
+			
+		}
+	}
+	
+
+}
